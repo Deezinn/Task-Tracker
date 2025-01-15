30d209612278cc2ab7f4a51ca2998ceec0653754
deezinn <developer.andrelz@gmail.com>
2025-01-14 22:25:33 -0300

    add: method add task

--- src/task.py
+++ src/task.py
@@ -1,13 +1,39 @@
+from time import gmtime, strftime
+
 class Task:
     def __init__(self):
-        self.task = {}
+        self.tasks = []
 
 
     def addTask(self):
 
+        task = {}
+        id_task = 1
+        descricao_task = str(input("Digite a descrição da Task: "))
+        status_task = str(input("Digite o status da task: "))
+        created_at = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+        updated_at = None
 
-    def updateTask(self):
+        task['id'] = id_task
+        task['Descrição'] = descricao_task
+        task['Status_task'] = status_task
+        task['Created_at'] = created_at
+        task['Updated_at'] = updated_at
+
+        self.tasks.append(task)
+        print(self.tasks)
 
 
-    def delTask(self):
 
+
+
+
+
+    # def updateTask(self):
+
+
+    # def delTask(self):
+
+
+task = Task()
+task.addTask()
